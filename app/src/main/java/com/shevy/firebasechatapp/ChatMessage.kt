93aaace7feeaa348e9a60250066e6f47b06e8259
Dmitriy Shevyakov <dmitriy_shevyakov@mail.ru>
2022-08-22 17:50:42 +0500

    Added layout of chat

---
+++ app/src/main/java/com/shevy/firebasechatapp/ChatMessage.kt
@@ -0,0 +1,12 @@
+package com.shevy.firebasechatapp
+
+class AwesomeMessage(
+    var text: String?,
+    var name: String?,
+    var imageUrl: String?,
+    var sender: String?,
+    var recipient: String?,
+    var isMine: Boolean
+) {
+
+}
