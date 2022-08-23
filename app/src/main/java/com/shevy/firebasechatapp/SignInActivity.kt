0495fe39279e94b05d3a76d1b64951b09b89a3bf
Dmitriy Shevyakov <dmitriy_shevyakov@mail.ru>
2022-08-23 17:02:44 +0500

    Added Firebase Authentication

--- app/src/main/java/com/shevy/firebasechatapp/SignInActivity.kt
+++ app/src/main/java/com/shevy/firebasechatapp/SignInActivity.kt
@@ -2,10 +2,17 @@ package com.shevy.firebasechatapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class SignInActivity : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_in)
+
+        auth = Firebase.auth
     }
 }
