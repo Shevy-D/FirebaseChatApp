d2183a1c068942eb6c0e019da5fd282448f3c396
Dmitriy Shevyakov <dmitriy_shevyakov@mail.ru>
2022-08-23 17:45:54 +0500

    Added Log In User

--- app/src/main/java/com/shevy/firebasechatapp/SignInActivity.kt
+++ app/src/main/java/com/shevy/firebasechatapp/SignInActivity.kt
@@ -3,6 +3,7 @@ package com.shevy.firebasechatapp
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
@@ -52,10 +53,12 @@ class SignInActivity : AppCompatActivity() {
 
         toggleLoginSignUpTextView.setOnClickListener {
             if (loginModeIsActive) {
+                repeatPasswordEditText.visibility = View.VISIBLE
                 loginModeIsActive = false
                 loginSignUpButton.text = "Sign Up"
                 toggleLoginSignUpTextView.text = "Tap to log in"
             } else {
+                repeatPasswordEditText.visibility = View.GONE
                 loginModeIsActive = true
                 loginSignUpButton.text = "Log In"
                 toggleLoginSignUpTextView.text = "Tap to sign up"
@@ -65,23 +68,46 @@ class SignInActivity : AppCompatActivity() {
     }
 
     private fun loginSignUpUser(email: String, password: String) {
-        auth.createUserWithEmailAndPassword(email, password)
-            .addOnCompleteListener(this) { task ->
-                if (task.isSuccessful) {
-                    // Sign in success, update UI with the signed-in user's information
-                    Log.d(TAG, "createUserWithEmail:success")
-                    val user = auth.currentUser
-                    //updateUI(user)
-                } else {
-                    // If sign in fails, display a message to the user.
-                    Log.w(TAG, "createUserWithEmail:failure", task.exception)
-                    Toast.makeText(
-                        this, "Authentication failed.",
-                        Toast.LENGTH_SHORT
-                    ).show()
-                    //updateUI(null)
+        if (loginModeIsActive) {
+            auth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        // Sign in success, update UI with the signed-in user's information
+                        Log.d(TAG, "signInWithEmail:success")
+                        val user = auth.currentUser
+                        //updateUI(user)
+
+                        startActivity(Intent(this, MainActivity::class.java))
+                    } else {
+                        // If sign in fails, display a message to the user.
+                        Log.w(TAG, "signInWithEmail:failure", task.exception)
+                        Toast.makeText(
+                            this, "Authentication failed.",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                        //updateUI(null)
+                    }
                 }
-            }
-        startActivity(Intent(this, MainActivity::class.java))
+        } else {
+            auth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        // Sign in success, update UI with the signed-in user's information
+                        Log.d(TAG, "createUserWithEmail:success")
+                        val user = auth.currentUser
+                        //updateUI(user)
+
+                        startActivity(Intent(this, MainActivity::class.java))
+                    } else {
+                        // If sign in fails, display a message to the user.
+                        Log.w(TAG, "createUserWithEmail:failure", task.exception)
+                        Toast.makeText(
+                            this, "Authentication failed.",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                        //updateUI(null)
+                    }
+                }
+        }
     }
 }
